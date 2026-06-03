70f8a380f58a8483732b0855bff5dcffb2d93b80
prince0538 <alexanderprincewill4@gmail.com>
2026-06-03 18:23:28 +0100

    update designer routes and API documentation for password reset functionality

--- routes/designer.js
+++ routes/designer.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 
-const { createDesingner, loginDesigner } = require('../controller/designer')
+const { createDesingner, loginDesigner, forgetPassword, setPassword } = require('../controller/designer')
 
 router.post('/', createDesingner);
 router.post('/login', loginDesigner);
+router.post('/forget-password', forgetPassword);
+router.post('/reset-password', setPassword);
 
 module.exports = router
