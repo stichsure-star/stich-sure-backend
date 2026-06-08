e25f7135456b171b3c782a620439916e36d63b00
prince0538 <alexanderprincewill4@gmail.com>
2026-06-08 16:10:49 +0100

    changes

--- routes/customer.js
+++ routes/customer.js
@@ -7,7 +7,7 @@ const { authentication } = require('../middlewares/authentication')
 const { authorizeRoles } = require('../middlewares/authorization')
 const { customerValidator, loginValidator, forgetPasswordValidator, resetPaswordValidator, updateCustomerProfileValidator, updatePasswordValidator } = require('../middlewares/customerValidation')
 
-router.post('/register', authentication, authorizeRoles('customer'), customerValidator, createCustomer);
+router.post('/register', authorizeRoles('customer'), customerValidator, createCustomer);
 router.post('/login', loginValidator, loginCustomer);
 router.post('/forget-password', forgetPasswordValidator, forgetPassword)
 router.post('/reset-password', authentication, authorizeRoles('customer'), resetPaswordValidator, resetPassword)
