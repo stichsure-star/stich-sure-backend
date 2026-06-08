71c87b455f06a2fbef929d521a8872a036d808b4
Brightstar <brightstarositadinma7@gmail.com>
2026-06-08 13:33:07 +0100

    Add role-based authorization to designer profile routes for enhanced security

--- routes/designerProfile.js
+++ routes/designerProfile.js
@@ -7,12 +7,13 @@ const {
   updateDesignerProfile,
   deleteDesignerProfile,
 } = require('../controller/designerProfile')
+const { authorizeRoles } = require('../middlewares/authorization') 
 const { authentication } = require('../middlewares/authentication')
 
-router.post('/create', authentication, upload.single('profilePhoto'), createOrUpdateDesignerProfile);
+router.post('/create', authentication, authorizeRoles('designer'), upload.single('profilePhoto'), createOrUpdateDesignerProfile);
 router.get('/getAll', getAllDesignerProfiles);
 router.get('/getByDesigner/:designerId', getDesignerProfile);
-router.put('/update', authentication,  upload.single('profilePhoto'), updateDesignerProfile);
-router.delete('/delete', authentication, deleteDesignerProfile);
+router.put('/update', authentication, authorizeRoles('designer'), upload.single('profilePhoto'), updateDesignerProfile);
+router.delete('/delete', authentication, authorizeRoles('designer'), deleteDesignerProfile);
 
 module.exports = router;
