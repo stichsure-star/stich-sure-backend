b198e10a4df7ada1ee9dd4fa6c46e6b2656cc108
Brightstar <brightstarositadinma7@gmail.com>
2026-06-10 09:33:07 +0100

    Add migration and model for Shipments table

---
+++ models/shipbubble.js
@@ -0,0 +1,31 @@
+const {Model} =  require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+  const Shipment = sequelize.define('Shipment', {
+    orderId: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    trackingCode: {
+      type: DataTypes.STRING,
+    },
+    trackingUrl: {
+      type: DataTypes.STRING,
+    },
+    courier: {
+      type: DataTypes.STRING,
+    },
+    status: {
+      type: DataTypes.STRING,
+      defaultValue: 'pending',
+    },
+    shippingFee: {
+      type: DataTypes.FLOAT,
+    },
+    currency: {
+      type: DataTypes.STRING,
+      defaultValue: 'NGN',
+    },
+  });
+
+  return Shipment;
+};
