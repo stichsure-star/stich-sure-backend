f10868b7d0e9878c6d7d01a0297ae80cca76eb0c
Brightstar <brightstarositadinma7@gmail.com>
2026-07-06 08:41:08 +0100

    Changes

---
+++ config/config.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+USERNAME = process.env.DB_USERNAME
+PASSWORD = process.env.DB_PASSWORD
+DATABASE = process.env.DB_DATABASE
+HOST = process.env.DB_HOST
+DIALECT = process.env.DB_DIALECT
+
+
+module.exports = {
+  "development": {
+    "username": USERNAME,
+    "password": PASSWORD,
+    "database": DATABASE,
+    "host": HOST,
+    "dialect": DIALECT
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": USERNAME,
+    "password": PASSWORD,
+    "database": DATABASE,
+    "host": HOST,
+    "dialect": DIALECT
+  }
+}
